fdca8c7ade5ea803f0a724808a2bc9d61558bf36
M-logique <lmy615730@gmail.com>
2025-02-24 10:38:52 +0330

    Refactor discloud_upload.py for improved readability and functionality

--- .github/discloud_upload.py
+++ .github/discloud_upload.py
@@ -25,7 +25,6 @@ if not API_TOKEN:
 client = discloud.Client(API_TOKEN)
 
 
-
 async def find_app_by_name(name: str) -> Optional[discloud.discloud.Application]:
 
     async with ClientSession() as session:
@@ -41,27 +40,32 @@ async def find_app_by_name(name: str) -> Optional[discloud.discloud.Application]
                 return None
 
             app_object = await client.app_info(app_id)
-            
+
             setattr(app_object, "name", name)
             setattr(app_object, "app_id", app_id)
             return app_object
 
-async def upload_app(zipfile_path: Path, app_id: Optional[str]) -> discloud.discloud.Action:
-    
+
+async def upload_app(
+    zipfile_path: Path, app_id: Optional[str]
+) -> discloud.discloud.Action:
+
     discloud_file: discloud.File = discloud.File(zipfile_path.__fspath__())
 
     if app_id is not None:
         return await client.commit(app_id, discloud_file)
-    
+
     return await client.upload_app(discloud_file)
+
 
 def read_discloud_config(filepath: Path) -> ConfigParser:
     with open(filepath, "r") as fp:
         content = f"[MAIN]\n{fp.read()}"
-    
+
     config = ConfigParser()
     config.read_string(content)
     return config
+
 
 def generate_zip_archive(source_path: Path, config_path: Path) -> Path:
     timestamp = dt.now().strftime("%Y%m%d_%H%M%S_%f")
@@ -70,18 +74,16 @@ def generate_zip_archive(source_path: Path, config_path: Path) -> Path:
     os.makedirs(workdir_name)
     workdir_path = Path(workdir_name)
     logger.info("Copying files to %s", workdir_name)
-    for file in source_path.glob("*"):
-        logger.info("Copied %s -> %s", file, workdir_path)
-        shutil.copy2(file, workdir_path)
-    
+    shutil.copytree(source_path, workdir_path)
+    logger.info("Copied files -> %s", workdir_path)
+
     shutil.copy2(config_path, workdir_path)
     logger.info("Copied config file -> %s", workdir_path)
 
     shutil.make_archive(workdir_name, "zip", workdir_path)
 
-    os.remove(workdir_path)
+    os.removedirs(workdir_path)
     return Path(f"{workdir_name}.zip")
-    
 
 
 async def main():
@@ -116,9 +118,9 @@ async def main():
     app = await find_app_by_name(app_name)
     app_id: Optional[str] = getattr(app, "app_id", None)
     if app:
-        logger.info("App %s found with id: %s", app.name, app.app_id)    
+        logger.info("App %s found with id: %s", app.name, app.app_id)
         logger.info("Commiting files to app: %s", app.name)
-        
+
     else:
         logger.info("App doesn't exists")
         logger.info("Uploading them to discloud")
